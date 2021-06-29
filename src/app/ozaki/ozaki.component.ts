2725b41310e8129e8b7d73ed24dbe050f81061d2
Tuyen4796 <37842539+Tuyen4796@users.noreply.github.com>
2021-06-29 16:51:29 +0700

    ozaki tesst

---
+++ src/app/ozaki/ozaki.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-ozaki',
+  templateUrl: './ozaki.component.html',
+  styleUrls: ['./ozaki.component.css']
+})
+export class OzakiComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
